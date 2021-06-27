b06a4d854489621568097f8efda95ce28cc8d8d9
Joao Felipe <brjoaof@gmail.com>
2021-06-27 12:09:20 -0300

    Add Share Button and Create Bitmap Function

--- app/src/main/java/com/devventure/colormyviews/MainActivity.kt
+++ app/src/main/java/com/devventure/colormyviews/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.devventure.colormyviews
 
+import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.devventure.colormyviews.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -28,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         loadColors()
+
 
     }
 
@@ -55,4 +69,62 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
+    fun share(view: View) {
+        val bitmap = getScreenShot(binding.container)
+        if (bitmap != null) {
+            saveScreenshot(bitmap)
+        }
+    }
+    private fun getScreenShot(v: View): Bitmap? {
+        var screenshot: Bitmap? = null
+
+        try {
+            screenshot =
+                Bitmap.createBitmap(v.measuredWidth, v.measuredHeight/2, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(screenshot)
+            v.draw(canvas)
+        } catch (e: Exception) {
+            Log.e("GFG", "Failed to capture screenshot. Error: "+ e.message)
+        }
+        return screenshot
+    }
+
+    private fun saveScreenshot(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                val imgUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imgUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imgDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imgDir, fileName)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this, R.string.image_captured, Toast.LENGTH_SHORT).show()
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_TEXT, R.string.checkout_screenshot)
+            shareIntent.type = "image/*"
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            startActivity(Intent.createChooser(shareIntent, R.string.share_via.toString()))
+        }
+    }
+
+
+
 }
