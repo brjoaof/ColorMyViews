1096f783eb6b387efa72caebab7fa31a572672cb
Joao Felipe <brjoaof@gmail.com>
2021-06-27 10:45:30 -0300

    Code Refactor

--- app/src/main/java/com/devventure/colormyviews/MainActivity.kt
+++ app/src/main/java/com/devventure/colormyviews/MainActivity.kt
@@ -4,18 +4,17 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-//import com.devventure.colormyviews.databinding.ActivityMainBinding
+import android.view.View
+import com.devventure.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var pencil: Int = R.color.gray
 
-    private var boxOneColor = R.color.gray
-    private var boxTwoColor = R.color.gray
-    private var boxThreeColor = R.color.gray
-    private var boxFourColor = R.color.gray
-    private var boxFiveColor = R.color.gray
+    private val boxes = arrayOf(
+        R.id.box_one_text, R.id.box_two_text, R.id.box_three_text,
+        R.id.box_four_text, R.id.box_five_text
+    )
 
     private val sharedPreferences : SharedPreferences
         get(){
@@ -24,81 +23,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        val view = binding.root
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-
-        var pencil = R.color.gray
-        val btnRed = findViewById<Button>(R.id.btn_red)
-        val btnGreen = findViewById<Button>(R.id.btn_green)
-        val btnYellow = findViewById<Button>(R.id.btn_yellow)
-
-        val boxOne = findViewById<TextView>(R.id.box_one_text)
-        val boxTwo = findViewById<TextView>(R.id.box_two_text)
-        val boxThree = findViewById<TextView>(R.id.box_three_text)
-        val boxFour = findViewById<TextView>(R.id.box_four_text)
-        val boxFive = findViewById<TextView>(R.id.box_five_text)
-
-        boxOneColor = sharedPreferences.getInt("boxOne", R.color.gray)
-        boxTwoColor = sharedPreferences.getInt("boxTwo", R.color.gray)
-        boxThreeColor = sharedPreferences.getInt("boxThree", R.color.gray)
-        boxFourColor = sharedPreferences.getInt("boxFour", R.color.gray)
-        boxFiveColor = sharedPreferences.getInt("boxFive", R.color.gray)
-
-        boxOne.setBackgroundResource(boxOneColor)
-        boxTwo.setBackgroundResource(boxTwoColor)
-        boxThree.setBackgroundResource(boxThreeColor)
-        boxFour.setBackgroundResource(boxFourColor)
-        boxFive.setBackgroundResource(boxFiveColor)
-
-        btnRed.setOnClickListener{
-            pencil = R.color.red
-        }
-
-        btnGreen.setOnClickListener{
-            pencil = R.color.green
-        }
-
-        btnYellow.setOnClickListener{
-            pencil = R.color.yellow
-        }
-
-        boxOne.setOnClickListener{
-            boxOne.setBackgroundResource(pencil)
-            boxOneColor = pencil
-        }
-
-        boxTwo.setOnClickListener{
-            boxTwo.setBackgroundResource(pencil)
-            boxTwoColor = pencil
-        }
-        boxThree.setOnClickListener{
-            boxThree.setBackgroundResource(pencil)
-            boxThreeColor = pencil
-        }
-        boxFour.setOnClickListener{
-            boxFour.setBackgroundResource(pencil)
-            boxFourColor = pencil
-        }
-        boxFive.setOnClickListener{
-            boxFive.setBackgroundResource(pencil)
-            boxFiveColor = pencil
-        }
+        loadColors()
 
     }
 
-    override fun onStop() {
-        super.onStop()
+    fun onButtonClick(view: View) {
+        pencil = when(view.id) {
+            R.id.btn_red -> R.color.red
+            R.id.btn_green -> R.color.green
+            R.id.btn_yellow -> R.color.yellow
+            else -> R.color.gray
+        }
+    }
+
+    fun onBoxClick(view: View) {
+        view.setBackgroundResource(pencil)
 
         val editor = sharedPreferences.edit()
-
-        editor.putInt("boxOne", boxOneColor)
-        editor.putInt("boxTwo", boxTwoColor)
-        editor.putInt("boxThree", boxThreeColor)
-        editor.putInt("boxFour", boxFourColor)
-        editor.putInt("boxFive", boxFiveColor)
-
+        editor.putInt("${view.id}", pencil)
         editor.commit()
+    }
+
+    private fun loadColors() {
+        for (box in boxes) {
+            findViewById<View>(box).setBackgroundResource(
+                sharedPreferences.getInt("$box", R.color.gray)
+            )
+        }
     }
 }
